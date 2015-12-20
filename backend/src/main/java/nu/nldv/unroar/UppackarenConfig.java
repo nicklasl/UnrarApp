fbb35658437f7edb84477a32698026aa5abddf32
Nicklas <nicklaslundin@gmail.com>
2015-12-20 10:07:19 +0100

    Clean up

--- backend/src/main/java/nu/nldv/unroar/UppackarenConfig.java
+++ backend/src/main/java/nu/nldv/unroar/UppackarenConfig.java
@@ -8,25 +8,10 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 public class UppackarenConfig {
 
-
-    /*
-    <bean id="taskScheduler"
-          class="org.springframework.scheduling.concurrent.ConcurrentTaskScheduler">
-    </bean>
-     */
     @Bean
     public ConcurrentTaskScheduler taskScheduler() {
         return new ConcurrentTaskScheduler();
     }
-
-    /*
-    <bean id="taskExecutor"
-          class="org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor">
-        <property name="corePoolSize" value="5" />
-        <property name="maxPoolSize" value="10" />
-        <property name="WaitForTasksToCompleteOnShutdown" value="true" />
-    </bean>
-     */
 
     @Bean
     public ThreadPoolTaskExecutor taskExecutor() {
